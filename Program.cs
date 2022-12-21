6158d3cb90c3de7adba290ad03d0531771775fd5
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-12-21 04:44:23 -0600

    Topic is landing page

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Topics}/{action=Index}/{id?}");
 
 app.Run();
