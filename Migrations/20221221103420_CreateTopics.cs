43acea520d8b6bb65c9638a2955c9e5caef4c242
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-12-21 04:38:24 -0600

    Migrations for Topic

---
+++ Migrations/20221221103420_CreateTopics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Highlights.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTopics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Topic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Premise = table.Column<string>(type: "TEXT", nullable: true),
+                    Goal = table.Column<string>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topic", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Topic");
+        }
+    }
+}
