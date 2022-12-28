6e52d38e7b9adc5066b37ed3a7df473a87b2348e
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-12-28 08:24:09 -0600

    One-to-many rels of Highlight Model

---
+++ Models/Highlight.cs
@@ -0,0 +1,11 @@
+namespace Highlights.Models;
+
+public class Highlight
+{
+    public string Text { get; set; } = null!;
+    public string? Comment { get; set; }
+    public int TopicId { get; set; }
+    public Topic Topic { get; set; }
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+}
