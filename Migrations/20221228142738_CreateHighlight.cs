ec58ae079090d27688281fba9ff3e756c21402ab
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-12-28 08:28:49 -0600

    Migration for Highlight model

---
+++ Migrations/20221228142738_CreateHighlight.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Highlights.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateHighlight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Highlight",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true),
+                    TopicId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Highlight", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Highlight_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Highlight_Topic_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Highlight_BookId",
+                table: "Highlight",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Highlight_TopicId",
+                table: "Highlight",
+                column: "TopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Highlight");
+        }
+    }
+}
